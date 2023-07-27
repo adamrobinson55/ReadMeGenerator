d82e6b371ca31a3631da3ead6d1448bad3770bb1
Adam R <adamrobinson55@gmail.com>
2023-07-27 13:19:24 -0500

    commiting

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# ReadMeGenerator
-Quickly generates a ReadMe file
+  
+# ReadMe Generator by Adam Robinson
+
+  [![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)
+## Description
+
+  To speed up the process of writing professional readmes. to better myself as a developer and save time in the future. 
+  the amount of time it takes to create readme files. I learned about creating applications and how to better create readme files.
+
+## Table of contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+## Installation
+  No steps required
+## Usage
+  Create professional readme files quickly
+## Credits
+  Adam Robinson
+## License
+  https://opensource.org/licenses/EPL-1.0
+## How to Contribute
+  Add more licenses
+## Tests
+  There are currently no tests
+## Questions
+  AdamRobinson55
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,80 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(!license){return ""}
+  switch(license) {
+    case 'Apache':
+      return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    case 'Boost':
+      return '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)'
+    case 'BSD':
+      return '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
+    case 'Creative Commons':
+      return '[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)'
+    case 'Eclipse':
+      return '[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)'
+    case 'GNU':
+      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(!license){return ""}
+  switch(license) {
+    case 'Apache':
+      return 'https://opensource.org/licenses/Apache-2.0'
+    case 'Boost':
+      return 'https://www.boost.org/LICENSE_1_0.txt'
+    case 'BSD':
+      return 'https://opensource.org/licenses/BSD-3-Clause'
+    case 'Creative Commons':
+      return 'http://creativecommons.org/publicdomain/zero/1.0/'
+    case 'Eclipse':
+      return 'https://opensource.org/licenses/EPL-1.0'
+    case 'GNU':
+      return 'https://www.gnu.org/licenses/gpl-3.0'
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(!license){return ""}
+  return renderLicenseLink(license)
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+return `  
+# ${data.title}
 
-`;
+  ${renderLicenseBadge(data.license)}
+## Description
+
+  ${data.motivated}. ${data.why}. 
+  ${data.problem}. ${data.learn}.
+
+## Table of contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+## Installation
+  ${data.install}
+## Usage
+  ${data.usage}
+## Credits
+  ${data.credits}
+## License
+  ${renderLicenseSection(data.license)}
+## How to Contribute
+  ${data.contribution}
+## Tests
+  ${data.tests}
+## Questions
+  ${data.github}`
 }
 
 module.exports = generateMarkdown;
